5b0f657fc38fcb7467bdc115bafe5333ddcfa9db
warren yun <xxwarrenhdxx@gmail.com>
2021-06-15 14:34:32 -0400

    Create initial project structure

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.container}>Midnight Hacks</h1>
+    </div>
+  )
+}
