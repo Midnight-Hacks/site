42dfd0401ca8e03f751bb27415877b7259251812
warren yun <xxwarrenhdxx@gmail.com>
2021-06-16 13:02:24 -0400

    Add sign up button, space everything oiut

--- components/Title.js
+++ components/Title.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import titleStyles from '../styles/Title.module.css';
 
 const Title = () => {
@@ -6,6 +7,7 @@ const Title = () => {
             <span className={titleStyles.title}>midnight hacks</span>
             <img src="/website-logo.png" className={titleStyles.logo}/>
             <h2 className={titleStyles.text}>A 12-hour student led hackathon designed for beginners!</h2>
+            <button className={titleStyles.btn}>Sign Up</button>
         </section>
     );
 }
