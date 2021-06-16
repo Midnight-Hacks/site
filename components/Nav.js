21060723a89561f59fdeba25f00dd37aa7c449d1
Fahim <fahimhussain21@gmail.com>
2021-06-16 16:36:52 -0400

    create static nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,58 @@
+import navStyles from '../styles/Nav.module.css';
+
 const Nav = () => {
-    return(
-        <div>
-        </div>
+    return (
+        <nav className={`relative flex flex-wrap items-center justify-between px-2 py-3 h-16 bg-charcoal-black navbar ${navStyles.navbar}`}>
+            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                    <a
+                        className={`inline-block whitespace-nowrap text-white ${navStyles.logo}`}
+                        href="#pablo"
+                    >
+                        <img src="/isolated-logo.png" className={`object-scale-down ${navStyles.logo}`}></img>
+                    </a>
+                    <button
+                        className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                        type="button"
+                    >
+                        <i className="fas fa-bars"></i>
+                    </button>
+                </div>
+                <div
+            className={
+              "lg:flex flex-grow items-center flex"
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-base leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-base leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-base leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Pin</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+            </div>
+        </nav>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Title from '../components/Title';
 import About from '../components/About';
+import Nav from '../components/Nav';
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       <Title />
       <About />
     </div>
