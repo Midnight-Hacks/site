32af353ba210d66ad72c1fa6290cb3fbe619cb6a
warren yun <xxwarrenhdxx@gmail.com>
2021-06-17 09:36:15 -0400

    Add content to about page

--- components/About.js
+++ components/About.js
@@ -5,13 +5,10 @@ const About = () => {
         <section className={aboutStyles.background}>
             <span className={aboutStyles.title}>about us</span>
             <p className={aboutStyles.text}>We are a group of student hackers that love coding!</p>
-            <div className={aboutStyles.logoContainer}>
+            <div className={aboutStyles.contentContainer}>
                 <img src="/logo.svg" alt="logo" className={aboutStyles.logo} />
+                <span className={aboutStyles.subtext}><strong>Midnight hacks</strong> is a 12-hour hackathon designed for beginner coders. Many schools in New York don't have hackathons hosted within their school, allowing students to miss out on memorable experiences, and opportunities to learn and build. <br /> <br />Our goal is to make a calm, low-pressure event for middle school and high school students in New York (and beyond) so they can learn to code, and gain memorable experiences along the way. You can work in up to teams of 4, and design and build a project from scratch. We'll also have workshops hosted throughout the event, so there's always an opportunity to learn something new.</span>
             </div>
-            <div className={aboutStyles.subtextContainer}>
-                <span className={aboutStyles.subtext}>Our goal is to make a calm, low-pressure event for middle school and high school to learn how to program.</span>
-            </div>
-            
         </section>
     );
 }
