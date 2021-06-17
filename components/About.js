574c22840bc9e82383c94e1f30359f7cefd1f030
warren yun <xxwarrenhdxx@gmail.com>
2021-06-16 22:55:18 -0400

    Add more content to about page

--- components/About.js
+++ components/About.js
@@ -4,7 +4,14 @@ const About = () => {
     return (
         <section className={aboutStyles.background}>
             <span className={aboutStyles.title}>about us</span>
-            <h2 className={aboutStyles.text}>We are a group of hackers that absolutely love coding</h2>
+            <p className={aboutStyles.text}>We are a group of student hackers that love coding!</p>
+            <div className={aboutStyles.logoContainer}>
+                <img src="/logo.svg" alt="logo" className={aboutStyles.logo} />
+            </div>
+            <div className={aboutStyles.subtextContainer}>
+                <span className={aboutStyles.subtext}>Our goal is to make a calm, low-pressure event for middle school and high school to learn how to program.</span>
+            </div>
+            
         </section>
     );
 }
