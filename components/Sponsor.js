3224fcda9b962a391cae41b2824a8cca88f9e3cc
warren yun <xxwarrenhdxx@gmail.com>
2021-06-17 11:18:41 -0400

    Fix logo color

--- components/Sponsor.js
+++ components/Sponsor.js
@@ -1,7 +1,7 @@
 const Sponsor = () => {
     return (
         <section>
-        
+            <h1>sponsors!</h1>
         </section>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Title from '../components/Title';
 import About from '../components/About';
+import Sponsor from '../components/Sponsor';
 
 export default function Home() {
   return (
     <div>
       <Title />
       <About />
+      <Sponsor />
     </div>
   )
 }
