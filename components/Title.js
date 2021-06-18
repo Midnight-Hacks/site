26c270dffd42dec1835c17a1c09bb64879ef91a9
warren yun <xxwarrenhdxx@gmail.com>
2021-06-17 22:02:43 -0400

    Fixed button bugs + tablet support (ish)

--- components/Title.js
+++ components/Title.js
@@ -10,9 +10,12 @@ const Title = () => {
                     <span className={titleStyles.title}>midnight hacks</span>
                     <img src="/website-logo.png" className={titleStyles.logo}/>
                     <h2 className={titleStyles.text}>A 12-hour student led hackathon designed for beginners!</h2>
-                    <button className={titleStyles.btn}>
-                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Sign up</a>
-                    </button>
+                    
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+                        <button className={titleStyles.btn}>
+                            Sign in
+                        </button>
+                    </a>
                 </div>
             </section>
         </>
