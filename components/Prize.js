ccdc69789d1b70a3c6138fd95387fb9a66573aab
warren yun <xxwarrenhdxx@gmail.com>
2021-06-18 19:01:41 -0400

    Complete prize component

--- components/Prize.js
+++ components/Prize.js
@@ -6,24 +6,58 @@ const Prize = () => {
             <br /> <br />
             <span className={prizeStyles.title}>prizes</span>
             <div className={prizeStyles.prizes}>
-                <div className={prizeStyles.prizeCard}>
-                    <span className={prizeStyles.text}>1st place</span>
+                <div className={prizeStyles.prizeDiv}>
+                    <div className={prizeStyles.prizeCard}>
+                        <span className={prizeStyles.text}>1st place</span>
+                    </div>
+                    <div className={prizeStyles.desc}>
+                        <span className={prizeStyles.subtext}>Coming soon!</span>
+                    </div>
                 </div>
-                <div className={prizeStyles.prizeCard}>
-                    <span className={prizeStyles.text}>2nd place</span>
+
+                <div className={prizeStyles.prizeDiv}>
+                    <div className={prizeStyles.prizeCard}>
+                        <span className={prizeStyles.text}>2nd place</span>
+                    </div>
+                    <div className={prizeStyles.desc}>
+                        <h1 className={prizeStyles.subtext}>Coming soon!</h1>
+                    </div>
                 </div>
-                <div className={prizeStyles.prizeCard}>
-                    <span className={prizeStyles.text}>3rd place</span>
+
+                <div className={prizeStyles.prizeDiv}>
+                    <div className={prizeStyles.prizeCard}>
+                        <span className={prizeStyles.text}>3rd place</span>
+                    </div>
+                    <div className={prizeStyles.desc}>
+                        <h1 className={prizeStyles.subtext}>Coming soon!</h1>
+                    </div>
                 </div>
-                <div className={prizeStyles.prizeCard}>
-                    <span className={prizeStyles.text}>best night hack</span>
+                <div className={prizeStyles.prizeDiv}>
+                    <div className={prizeStyles.prizeCard}>
+                        <span className={prizeStyles.text}>best night hack</span>
+                    </div>
+                    <div className={prizeStyles.desc}>
+                        <h1 className={prizeStyles.subtext}>Coming soon!</h1>
+                    </div>
                 </div>
-                <div className={prizeStyles.prizeCard}>
-                    <span className={prizeStyles.text}>best beginners hack</span>
+
+                <div className={prizeStyles.prizeDiv}>
+                    <div className={prizeStyles.prizeCard}>
+                        <span className={prizeStyles.text}>best beginners hack</span>
+                    </div>
+                    <div className={prizeStyles.desc}>
+                        <h1 className={prizeStyles.subtext}>Coming soon!</h1>
+                    </div>
                 </div>
-                <div className={prizeStyles.prizeCard}>
-                    <span className={prizeStyles.text}>funniest hack</span>
-                </div>-
+
+                <div className={prizeStyles.prizeDiv}>
+                    <div className={prizeStyles.prizeCard}>
+                        <span className={prizeStyles.text}>funniest hack</span>
+                    </div>
+                    <div className={prizeStyles.desc}>
+                        <h1 className={prizeStyles.subtext}>Coming soon!</h1>
+                    </div>
+                </div>
             </div>
         </section>
     );
