94e3e29215dafafee16c4d67c867181f51527435
warren yun <xxwarrenhdxx@gmail.com>
2021-06-19 09:30:02 -0400

    Add team member descriptions

--- components/Team.js
+++ components/Team.js
@@ -2,21 +2,29 @@ import teamStyles from '../styles/Team.module.css';
 
 const Team = () => {
     return (
-        <section className={teamStyles.background} id="team">
-            <span className={teamStyles.title}>our team</span>
+        <section className={teamStyles.background}>
+            <span className={teamStyles.title} id="team">our team</span>
             <div className={teamStyles.teamShowcase}>
                 <div className={teamStyles.member}>
-                    <img src="/warren.PNG" className={teamStyles.memberImage} />
+                    <img src="/members/warren.PNG" className={teamStyles.memberImage} />
                     <h1 className={teamStyles.text}>warren</h1>
+                    <h1 className={teamStyles.subtext}>Founder</h1>
                 </div>
                 <div className={teamStyles.member}>
-                    <img src="/placeholder.PNG" className={teamStyles.memberImage} />
+                    <img src="/members/placeholder.PNG" className={teamStyles.memberImage} />
                     <h1 className={teamStyles.text}>fahim</h1>
+                    <h1 className={teamStyles.subtext}>Co-founder</h1>
                 </div>
                 <div className={teamStyles.member}>
-                    <img src="/placeholder.PNG" className={teamStyles.memberImage} />
+                    <img src="/members/placeholder.PNG" className={teamStyles.memberImage} />
                     <h1 className={teamStyles.text}>asa</h1>
+                    <h1 className={teamStyles.subtext}>Co-founder</h1>
                 </div>
+                <div className="member member5">
+                    <img src="/members/jake.png" className={teamStyles.memberImage} />
+                <h1 className={teamStyles.text}>Jake</h1>
+                <h1 className={teamStyles.subtext}>twitch.tv/JakeJack</h1>
+            </div>
             </div>
         </section>
     );
