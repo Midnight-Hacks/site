59fbf5e6e36c4123e9e8bf3847c6c2d23bd4cac0
warren yun <xxwarrenhdxx@gmail.com>
2021-06-19 09:09:22 -0400

    Add members to team section

--- components/Team.js
+++ components/Team.js
@@ -4,6 +4,20 @@ const Team = () => {
     return (
         <section className={teamStyles.background} id="team">
             <span className={teamStyles.title}>our team</span>
+            <div className={teamStyles.teamShowcase}>
+                <div className={teamStyles.member}>
+                    <img src="/warren.PNG" className={teamStyles.memberImage} />
+                    <h1 className={teamStyles.text}>warren</h1>
+                </div>
+                <div className={teamStyles.member}>
+                    <img src="/placeholder.PNG" className={teamStyles.memberImage} />
+                    <h1 className={teamStyles.text}>fahim</h1>
+                </div>
+                <div className={teamStyles.member}>
+                    <img src="/placeholder.PNG" className={teamStyles.memberImage} />
+                    <h1 className={teamStyles.text}>asa</h1>
+                </div>
+            </div>
         </section>
     );
 }
