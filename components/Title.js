e7dab2c31416d80a473c883e016ea68725cba26d
warren yun <xxwarrenhdxx@gmail.com>
2021-06-20 13:02:08 -0400

    Update sign up button

--- components/Title.js
+++ components/Title.js
@@ -12,7 +12,7 @@ const Title = () => {
                     <h2 className={titleStyles.text}>A 12-hour student led hackathon designed for beginners!</h2>     
                     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
                         <button className={titleStyles.btn}>
-                            Sign up
+                            Coming soon
                         </button>
                     </a>
                 </div>
