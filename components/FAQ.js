f8eccb32c808e536232d5a0f069d69125c760f3b
warren yun <xxwarrenhdxx@gmail.com>
2021-06-20 10:26:18 -0400

    Add question cards to FAQ component

--- components/FAQ.js
+++ components/FAQ.js
@@ -6,7 +6,60 @@ const FAQ = () => {
             <br /> <br />
             <span className={faqStyles.title}>FAQ</span>
             <div className={faqStyles.questions}>
-                
+                <div className={faqStyles.quesDiv}>
+                    <div className={faqStyles.quesCard}>
+                        <span className={faqStyles.text}>what is a hackathon?</span>
+                    </div>
+                    <div className={faqStyles.quesDesc}>
+                        <span className={faqStyles.subtext}>Coming soon!</span>
+                    </div>
+                </div>
+
+                <div className={faqStyles.quesDiv}>
+                    <div className={faqStyles.quesCard}>
+                        <span className={faqStyles.text}>who can join?</span>
+                    </div>
+                    <div className={faqStyles.quesDesc}>
+                        <span className={faqStyles.subtext}>Coming soon!</span>
+                    </div>
+                </div>
+
+
+                <div className={faqStyles.quesDiv}>
+                    <div className={faqStyles.quesCard}>
+                        <span className={faqStyles.text}>do i need to know how to code?</span>
+                    </div>
+                    <div className={faqStyles.quesDesc}>
+                        <span className={faqStyles.subtext}>Nope! This event was specifically designed for beginners to get into coding.</span>
+                    </div>
+                </div>
+
+                <div className={faqStyles.quesDiv}>
+                    <div className={faqStyles.quesCard}>
+                        <span className={faqStyles.text}>is it free?</span>
+                    </div>
+                    <div className={faqStyles.quesDesc}>
+                        <span className={faqStyles.subtext}>Yes! The event is completely free.</span>
+                    </div>
+                </div>
+
+                <div className={faqStyles.quesDiv}>
+                    <div className={faqStyles.quesCard}>
+                        <span className={faqStyles.text}>who can join?</span>
+                    </div>
+                    <div className={faqStyles.quesDesc}>
+                        <span className={faqStyles.subtext}>Coming soon!</span>
+                    </div>
+                </div>
+
+                <div className={faqStyles.quesDiv}>
+                    <div className={faqStyles.quesCard}>
+                        <span className={faqStyles.text}>who can join?</span>
+                    </div>
+                    <div className={faqStyles.quesDesc}>
+                        <span className={faqStyles.subtext}>Coming soon!</span>
+                    </div>
+                </div>
             </div>
         </section>  
     );
--- components/Sponsor.js
+++ components/Sponsor.js
@@ -12,7 +12,6 @@ const Sponsor = () => {
                     Sponsor us!
                 </button>
             </a>
-            
         </section>
     );
 }
