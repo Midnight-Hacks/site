cc452984ee21eaf0a6f29ed27d31d835c0b5370c
warren yun <xxwarrenhdxx@gmail.com>
2021-06-25 04:40:19 -0400

    Add videos to resources component

--- components/Resources.js
+++ components/Resources.js
@@ -3,8 +3,35 @@ import resourceStyles from '../styles/Resources.module.css';
 const Resources = () => {
     return (
         <section className={resourceStyles.background}>
-            <br id="resource" />
-            <span className={resourceStyles.title}>Resources</span>
+            <br id="resources" />
+            <h1 className={resourceStyles.title}>Resources</h1>
+            <h1 className={resourceStyles.text}>New to coding? Check out these resources below!</h1>
+            <div className={resourceStyles.links}>
+                <div className={resourceStyles.frameDiv}>
+                    <iframe
+                        src="https://www.youtube.com/embed/rfscVS0vtbw"
+                        className={resourceStyles.video}>
+                    </iframe>
+                </div>
+                <div className={resourceStyles.frameDiv}>
+                    <iframe
+                        src="https://www.youtube.com/embed/pQN-pnXPaVg"
+                        className={resourceStyles.video}>
+                    </iframe>
+                </div>
+                <div className={resourceStyles.frameDiv}>
+                    <iframe
+                        src="https://www.youtube.com/embed/r3GGV2TG_vw"
+                        className={resourceStyles.video}>
+                    </iframe>
+                </div>
+                <div className={resourceStyles.frameDiv}>
+                    <iframe
+                        src="https://www.youtube.com/embed/PkZNo7MFNFg"
+                        className={resourceStyles.video}>
+                    </iframe>
+                </div>
+            </div>
             
         </section>
     );
