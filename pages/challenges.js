63a4cbfa7589603a1b233a09b9378c31abd9e76b
Fahim <fahimhussain21@gmail.com>
2021-07-01 14:28:04 -0400

    added challenges route

---
+++ pages/challenges.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Nav from '../components/Nav';
+
+import challengeStyles from '../styles/Challenges.module.css'
+
+export default function ChallengePage() {
+    return (
+        <div>
+            <Head>
+                <title>Challenges</title>
+            </Head>
+            <Nav />
+            <br id="start"></br>
+            <br></br>
+            <br></br>
+            <span className={challengeStyles.title}>challenges</span>
+        </div>
+    )
+}
