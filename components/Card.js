9a47d3040538b514d45b43d99a1c033cc639d987
Fahim <fahimhussain21@gmail.com>
2021-07-03 04:17:08 -0400

    created card layout and design works on mobile devices

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,8 @@ import cardStyles from '../styles/Card.module.css';
 
 const Card = () => (
     <div className={cardStyles.cardbase}>
-
+        <div className={cardStyles.cardTitle}>Go Demo!</div>
+        <h4 className={cardStyles.cardDescription}>Live demo your project at the end of the hackathon at one of our voice channels!</h4>
     </div>
 )
 
