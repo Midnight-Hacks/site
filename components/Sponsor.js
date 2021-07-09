c59e3045e792711ddbbe10eb14e4aaf5b986563b
warren yun <xxwarrenhdxx@gmail.com>
2021-07-09 13:42:35 -0400

    Add first sponsor logo

--- components/Sponsor.js
+++ components/Sponsor.js
@@ -5,8 +5,11 @@ const Sponsor = () => {
         <section className={sponsorStyles.background} id="sponsors">
             <br /> <br />
             <span className={sponsorStyles.title}>sponsors</span>
-            <h3 className={sponsorStyles.text}>404</h3>
-            <h3 className={sponsorStyles.text}>sponsor us <br></br>please</h3>
+            <div className={sponsorStyles.sponsorList}>
+                <a href="https://artofproblemsolving.com/" target="_blank">
+                    <img src="/sponsors/aops.png" alt="the art of problem solving" className={sponsorStyles.sponsorLogo}/>
+                </a>
+            </div>
             <a href="mailto:warren@midnighthacks.tech?subject=Sponsorship%20Interest">
                 <button className={sponsorStyles.btn}>
                     Sponsor us!
