67918a568f914e682bb098d32a13aa4d5a35f9eb
warren yun <xxwarrenhdxx@gmail.com>
2021-07-09 14:13:51 -0400

    Add hack club bank

--- components/Sponsor.js
+++ components/Sponsor.js
@@ -9,6 +9,10 @@ const Sponsor = () => {
                 <a href="https://artofproblemsolving.com/" target="_blank">
                     <img src="/sponsors/aops.png" alt="the art of problem solving" className={sponsorStyles.sponsorLogo}/>
                 </a>
+                <a href="https://hackclub.com/bank/" target="_blank">
+                    <img src="/sponsors/hack_club_bank.png" alt="the art of problem solving" className={sponsorStyles.sponsorLogo}/>
+                </a>
+
             </div>
             <a href="mailto:warren@midnighthacks.tech?subject=Sponsorship%20Interest">
                 <button className={sponsorStyles.btn}>
