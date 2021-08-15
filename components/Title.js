8c13a868aa9145378de3ab8d11e9db89caa81b59
warren yun <xxwarrenhdxx@gmail.com>
2021-08-15 11:57:59 -0400

    Set date back

--- components/Title.js
+++ components/Title.js
@@ -8,7 +8,7 @@ const Title = () => {
             <section className={titleStyles.background}>
                 <div className={titleStyles.content}>
                     <span className={titleStyles.title}>midnight hacks</span>
-                    <h1 className={titleStyles.date}>November 13th, 2021</h1>
+                    <h1 className={titleStyles.date}>November 6th, 2021</h1>
                     <img src="/website-logo.png" className={titleStyles.logo}/>
                     <h2 className={titleStyles.text}>A 24-hour student led hackathon designed for beginners!</h2>     
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSflRidy8ZQyrqGaEfpUfzOwfV-IL_FPXRYe3zby4cXFbS6IHg/viewform?usp=sf_link" target="_blank">
