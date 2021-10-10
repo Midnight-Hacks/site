52e7675882fcb72849167714f11d670abd16f18a
warren yun <xxwarrenhdxx@gmail.com>
2021-10-09 22:52:23 -0400

    Create schedule component

---
+++ components/Schedule.js
@@ -0,0 +1,11 @@
+import scheduleStyles from '../styles/Schedule.module.css';
+
+const Schedule = () => {
+    return (
+        <div>
+        
+        </div>
+    );
+}
+
+export default Schedule;
