9efffeada27a0cbdc4ad704a1ea2fb6f44f6abd5
warren yun <xxwarrenhdxx@gmail.com>
2021-11-06 15:24:20 -0400

    update schedule again oops

--- components/Schedule.js
+++ components/Schedule.js
@@ -45,7 +45,7 @@ const eventsDay2 = [
         desc: 'Don\'t miss Abigayle\'s talk on taking your hackathon project a step further!'
     },
     {
-        event: 'Building a CLI! - 1:30 PM',
+        event: 'Building a CLI! - 3:30 PM',
         desc: 'Check out Ishan\'s workshop on building a CLI! You don\'t want to miss it!'
     },
     {
