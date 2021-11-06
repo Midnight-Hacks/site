33a51046da9711173b8f1443a02c08980c27cad3
warren yun <xxwarrenhdxx@gmail.com>
2021-11-06 16:37:56 -0400

    Fix challenges website

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,9 @@
 import cardStyles from '../styles/Card.module.css';
 
-const Card = () => (
+const Card = ({name, desc}) => (
     <div className={cardStyles.cardbase}>
-        <div className={cardStyles.cardTitle}>Go Demo!</div>
-        <h4 className={cardStyles.cardDescription}>Live demo your project at the end of the hackathon at one of our voice channels!</h4>
+        <div className={cardStyles.cardTitle}>{name}</div>
+        <h4 className={cardStyles.cardDescription}>{desc}</h4>
     </div>
 )
 
--- pages/challenges.js
+++ pages/challenges.js
@@ -5,6 +5,30 @@ import Card from '../components/Card';
 import challengeStyles from '../styles/Challenges.module.css';
 import titleStyles from '../styles/Title.module.css';
 
+const challenges = [
+
+    {
+        name: 'Object Oriented Program',
+        desc: 'Project'
+    },
+    {
+        name: 'Object Oriented Program',
+        desc: 'Project'
+    },
+    {
+        name: 'Object Oriented Program',
+        desc: 'Project'
+    },
+    {
+        name: 'Object Oriented Program',
+        desc: 'Project'
+    },
+    {
+        name: 'Object Oriented Program',
+        desc: 'Project'
+    },
+]
+
 export default function ChallengePage() {
     return (
         <div>
@@ -18,12 +42,9 @@ export default function ChallengePage() {
             <span className={challengeStyles.title}>challenges</span>
             <h2 className={titleStyles.text}>Coming soon :)</h2>
             <section className={challengeStyles.cardSection}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {challenges.map((chal, i) => (
+                    <Card key={i} name={chal.name} desc={chal.desc} />
+                ))}
             </section>
         </div>
     )
